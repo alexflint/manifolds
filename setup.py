707b629f187123be511e972b6083aa4919a950bb
Alex Flint <alex.flint@gmail.com>
2015-07-05 20:17:21 -0700

    Release version 0.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name='manifolds',
       description='Utilities for working with differentiable manifolds',
-      version='0.11',
+      version='0.12',
       author='Alex Flint',
       author_email='alex.flint@gmail.com',
       url='https://github.com/alexflint/manifolds',
